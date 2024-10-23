d207d45045293af0a9f77c6f2aa1212af17cd17a
Joakim Sommerseth Kjellmo <Joakim.Kjellmo@tromso.serit.no>
2024-10-23 08:12:02 +0200

    eslint_d isn't working properly, lspconfig should be good, leave MASON be if you want HTML snippets in vue

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,6 +30,7 @@ return {
         filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
       })
 
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
