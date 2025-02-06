4dffba3d27de0f0221c43be94d5fb7fafd57bcc6
J-Kjellmo <joamath@outlook.com>
2025-02-06 16:27:02 +0100

    Indent lines and numbertoggle

---
+++ lua/plugins/nvim-numbertoggle.lua
@@ -0,0 +1,23 @@
+return {
+  "sitiom/nvim-numbertoggle",
+  config = function()
+    local numbertoggle_enabled = true
+
+    function ToggleNumberToggle()
+      numbertoggle_enabled = not numbertoggle_enabled
+      if numbertoggle_enabled then
+        vim.cmd("doautocmd User NumberToggle")  -- Trigger plugin behavior
+        print("nvim-numbertoggle: Enabled")
+      else
+        vim.opt.number = true
+        vim.opt.relativenumber = false
+        vim.cmd("autocmd! * <buffer>") -- Clear buffer-specific autocmds
+        print("nvim-numbertoggle: Disabled")
+      end
+    end
+
+    vim.api.nvim_create_user_command("ToggleNumberToggle", ToggleNumberToggle, {})
+    vim.keymap.set('n', '<C-l>', ':ToggleNumberToggle<CR>', { noremap = true, silent = true })
+  end
+}
+
